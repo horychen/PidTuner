7e70114b7dd922e6a772e04e17b2e1e2aa2d222e
Jiahao Chen <horychen@qq.com>
2020-04-23 17:56:18 +0800

    Add tuner and jupyter lab file.

---
+++ tuner.py
@@ -0,0 +1,47 @@
+from pylab import np, plt
+
+def get_coeffs_dc_motor_current_regulator(R, L, Bandwidth_Hz):
+    Kp = Bandwidth_Hz * 2 * np.pi * L
+    Ki = R / L
+    return Kp, Ki
+
+import control
+from control import bode_plot
+def display(x):
+    pass
+
+R = 3 # Ohm
+L = 0.4 # H
+
+Kp, Ki = get_coeffs_dc_motor_current_regulator(R, L, 200)
+
+dc_motor = control.tf([1], [L, R])
+pi_regulator = control.tf([Kp, Kp*Ki], [1, 0])
+display(dc_motor)
+display(pi_regulator)
+# figure()
+# mag, phase, omega = bode_plot(dc_motor, 2*np.pi*np.logspace(-2,4,1000), dB=1, Hz=1, deg=1)
+# figure()
+# mag, phase, omega = bode_plot(pi_regulator, 2*np.pi*np.logspace(-2,4,1000), dB=1, Hz=1, deg=1)
+
+open_sys = control.series(pi_regulator, dc_motor)
+closed_sys = control.feedback(dc_motor, pi_regulator, sign=-1)
+
+# open_sys = pi_regulator * dc_motor
+closed_sys = open_sys / (1+open_sys)
+
+display(open_sys)
+display(control.minreal(closed_sys))
+closed_sys = control.minreal(closed_sys)
+
+# figure()
+# mag, phase, omega = bode_plot(open_sys, 2*np.pi*np.logspace(-2,4,1000), dB=1, Hz=1, deg=1)
+plt.figure()
+mag, phase, omega = bode_plot(closed_sys, 2*np.pi*np.logspace(-2,4,1000), dB=1, Hz=1, deg=1)
+
+T, yout = control.step_response(closed_sys, np.arange(0,2,1e-4))
+plt.figure()
+plt.plot(T, yout)
+
+plt.show()
+
